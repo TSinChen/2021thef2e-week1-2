8bd828fe895b2f69faa90ba48b0ea9032fb68291
TSinChen <sinin60076@gmail.com>
2022-07-16 16:22:15 +0800

    feat: footer (#5)

---
+++ src/containers/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="h-[60px] flex justify-center items-center bg-[#65895F] text-[#fff] text-[14px]">
+      <span>#The F2E 3rd Week01 #Breakfast</span>
+      <a
+        href="https://github.com/TSinChen"
+        target="_blank"
+        rel="noreferrer"
+        className="ml-[5px] underline"
+      >
+        #TSinChen
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
