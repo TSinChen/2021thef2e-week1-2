ac2ec9965e5d085738aa44114f29732b71bb00ef
TSinChen <sinin60076@gmail.com>
2022-07-16 15:58:12 +0800

    feat: main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import { Fragment } from "react";
 
 import "./styles/index.css";
 import Header from "./containers/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./containers/MainPage/MainPage";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <BrowserRouter>
+        <main className="max-w-[1200px] mx-auto px-[45px]">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </Fragment>
   );
 }
