30cbb42993f48a5851a797574c50d606d1b0ae6f
TSinChen <sinin60076@gmail.com>
2022-07-16 15:42:10 +0800

    chore: fix style

--- src/containers/MainPage/TitleSearch/TitleSearch.tsx
+++ src/containers/MainPage/TitleSearch/TitleSearch.tsx
@@ -3,7 +3,7 @@ import Title from "./Title";
 
 const TitleSearch = () => {
   return (
-    <div className="flex justify-between pt-[82px] pb-[57px]">
+    <div className="flex justify-between pt-[82px] pb-[21px]">
       <Title />
       <Search />
     </div>
