f44859e78ed0cd59e3f2d54d98aa825272df5e75
TSinChen <sinin60076@gmail.com>
2022-07-17 17:50:10 +0800

    feat: add links (#13)

--- src/containers/MainPage/RecentActivity/RecentActivity.tsx
+++ src/containers/MainPage/RecentActivity/RecentActivity.tsx
@@ -4,7 +4,7 @@ import ActivityList from "./ActivityList/ActivityList";
 const RecentActivity = () => {
   return (
     <div>
-      <Title title="近期活動" linkText="查看更多活動" linkHref="/" />
+      <Title title="近期活動" linkText="查看更多活動" linkHref="/Activity" />
       <ActivityList />
     </div>
   );
