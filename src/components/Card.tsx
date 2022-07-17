e2e13069edee74a9d06b7a3df1391e511c4f1350
TSinChen <sinin60076@gmail.com>
2022-07-17 17:30:24 +0800

    chore: remove return

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,31 +11,29 @@ type Props = {
   liStyle?: string;
 };
 
-const Card = ({ type, id, pictureUrl, name, city, liStyle }: Props) => {
-  return (
-    <li
-      className={`w-[255px] mr-[30px] [&:nth-child(4n)]:mr-0 group${
-        liStyle ? ` ${liStyle}` : ""
-      }`}
-    >
-      <a href={`/${type}/${id}`}>
-        <div className="w-full h-[200px] mb-[10px] rounded-[20px] overflow-hidden">
-          <img
-            src={pictureUrl}
-            alt={name}
-            className="w-full h-full object-cover group-hover:scale-110 duration-200"
-          />
-        </div>
-        <p className="mb-[6px] text-[22px] leading-[32px] font-bold text-custom-gray-2 line-clamp-1">
-          {name}
-        </p>
-        <p className="flex text-custom-gray-1">
-          <img src={POINTER} alt="" className="mr-[4px]" />
-          <span>{city}</span>
-        </p>
-      </a>
-    </li>
-  );
-};
+const Card = ({ type, id, pictureUrl, name, city, liStyle }: Props) => (
+  <li
+    className={`w-[255px] mr-[30px] [&:nth-child(4n)]:mr-0 group${
+      liStyle ? ` ${liStyle}` : ""
+    }`}
+  >
+    <a href={`/${type}/${id}`}>
+      <div className="w-full h-[200px] mb-[10px] rounded-[20px] overflow-hidden">
+        <img
+          src={pictureUrl}
+          alt={name}
+          className="w-full h-full object-cover group-hover:scale-110 duration-200"
+        />
+      </div>
+      <p className="mb-[6px] text-[22px] leading-[32px] font-bold text-custom-gray-2 line-clamp-1">
+        {name}
+      </p>
+      <p className="flex text-custom-gray-1">
+        <img src={POINTER} alt="" className="mr-[4px]" />
+        <span>{city}</span>
+      </p>
+    </a>
+  </li>
+);
 
 export default Card;
