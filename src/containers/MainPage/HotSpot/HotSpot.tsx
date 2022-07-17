8f8ef3c68d156a786d700f69b0ffc6a7639c50be
TSinChen <sinin60076@gmail.com>
2022-07-17 17:49:22 +0800

    feat: add links

--- src/containers/MainPage/HotSpot/HotSpot.tsx
+++ src/containers/MainPage/HotSpot/HotSpot.tsx
@@ -4,7 +4,11 @@ import HotSpotList from "./HotSpotList/HotSpotList";
 const HotSpot = () => {
   return (
     <div>
-      <Title title="熱門打卡景點" linkText="查看更多景點" linkHref="/" />
+      <Title
+        title="熱門打卡景點"
+        linkText="查看更多景點"
+        linkHref="/ScenicSpot"
+      />
       <HotSpotList />
     </div>
   );
--- src/containers/MainPage/Restaurant/Restaurant.tsx
+++ src/containers/MainPage/Restaurant/Restaurant.tsx
@@ -4,7 +4,11 @@ import RestaurantList from "./RestaurantList/RestaurantList";
 const Restaurant = () => {
   return (
     <div>
-      <Title title="一再回訪美食" linkText="查看更多美食" linkHref="/" />
+      <Title
+        title="一再回訪美食"
+        linkText="查看更多美食"
+        linkHref="/Restaurant"
+      />
       <RestaurantList />
     </div>
   );
